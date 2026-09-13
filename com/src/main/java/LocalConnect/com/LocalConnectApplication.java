8e53e6e582590b8015e804467ee3fd1cdf0d1e40
keshavjanglwa <keshavjanglwa@gmail.com>
2026-09-13 14:32:19 +0530

    first commit

---
+++ com/src/main/java/LocalConnect/com/LocalConnectApplication.java
@@ -0,0 +1,14 @@
+package LocalConnect.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalConnectApplication.class, args);
+		System.out.println("LocalConnectApplication is running...");
+	}
+
+}
